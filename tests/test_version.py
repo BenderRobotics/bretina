c190476ce4f804a8071fb3743d4f8b8b8f870ea6
Pavel Kumpán <kumpan@benderrobotics.com>
2019-12-16 15:12:39 +0100

    fix version test (refs #2631)

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,7 +4,7 @@ import bretina
 curr_ver = bretina.__version__
 
 if 'CI_COMMIT_TAG' in os.environ:
-    exp_ver = os.environ['CI_COMMIT_TAG']
+    exp_ver = os.environ['CI_COMMIT_TAG'].strip('v')
 
     msg = 'Version mismatch! (Current={0} != Expected={1})'.format(curr_ver, exp_ver)
     assert curr_ver == exp_ver, msg
