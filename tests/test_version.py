663dda756251a62c2168e8eb39fb6f363e466baa
Pavel Kumpán <kumpan@benderrobotics.com>
2019-12-16 14:21:24 +0100

    add gitlab CI (refs #2631)

---
+++ tests/test_version.py
@@ -0,0 +1,12 @@
+import os
+import bretina
+
+curr_ver = bretina.__version__
+
+if 'CI_COMMIT_TAG' in os.environ:
+    exp_ver = os.environ['CI_COMMIT_TAG']
+
+    msg = 'Version mismatch! (Current={0} != Expected={1})'.format(curr_ver, exp_ver)
+    assert curr_ver == exp_ver, msg
+else:
+    print(f"Version {curr_ver} can not be verified since not runned from gitlab CI")
