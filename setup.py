c97ed312522892734de0d91290c94dfd0dd488b1
Dominik Muller <muller@benderrobotics.com>
2024-10-23 17:21:43 +0200

    Add pypi deploy job

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setuptools.setup(
         'numpy>=1.17',
         'pytesseract>=0.3.0',
         'Pillow>=6.2.1',
-        'htmllogging @ https://pypi.benderrobotics.com/packages/htmllogging-0.0.1.tar.gz'
+        'htmllogging>=0.0.1',
     ]
 )
